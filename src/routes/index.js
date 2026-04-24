7a642fd9671dd2720c6088c421281bc546f9950c
JELASCO <jelomarketplace@gmail.com>
2026-04-24 17:12:01 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-{"error":"Not Found"}const express = require('express');
+const express = require('express');
 const router = express.Router();
 const _rateLimit = require('express-rate-limit');
 const rateLimit = _rateLimit.default || _rateLimit;
